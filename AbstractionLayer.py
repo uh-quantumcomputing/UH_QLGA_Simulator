9d877f49421ec95417ddea143c06c64eb4705bc0
ssmith22 <ssmith22@hawaii.edu>
2019-07-17 12:08:02 -1000

    First version

---
+++ AbstractionLayer.py
@@ -0,0 +1,86 @@
+import Code.Simulations.SimulationHelper as sim
+
+PARTICLES = None
+KINETIC_OPERATOR = None
+FRAME_SIZE = None
+NUM_FRAMES = None
+Lx = None  
+Ly = None 
+Lz = None 
+BATCH = None
+RUN = None
+DEVICES = None
+INIT = None
+MODEL = None
+EXP_KWARGS = None
+POTENTIAL = None
+POTENTIAL_KWARGS = None
+VISUALIZATION = None 
+VIS_KWARGS = None
+MEASUREMENT = None
+MEASUREMENT_KWARGS = None
+
+def setup(particles, kinetic_operator, frame_size, num_frames, lx,  ly, lz, 
+			batch, run, devices, init, model, exp_kwargs, potential, potential_kwargs,
+			visualization, vis_kwargs, measurement, measurement_kwargs):
+	global PARTICLES, KINETIC_OPERATOR, FRAME_SIZE, NUM_FRAMES, Lx,  Ly, Lz 
+	global BATCH, RUN, DEVICES, INIT, MODEL, EXP_KWARGS, POTENTIAL, POTENTIAL_KWARGS
+	global VISUALIZATION, VIS_KWARGS, MEASUREMENT, MEASUREMENT_KWARGS
+	PARTICLES, KINETIC_OPERATOR, FRAME_SIZE, NUM_FRAMES, Lx,  Ly, Lz = particles, kinetic_operator, frame_size, num_frames, lx,  ly, lz 
+	BATCH, RUN, DEVICES, INIT, MODEL, EXP_KWARGS, POTENTIAL, POTENTIAL_KWARGS = batch, run, devices, init, model, exp_kwargs, potential, potential_kwargs
+	VISUALIZATION, VIS_KWARGS, MEASUREMENT, MEASUREMENT_KWARGS = visualization, vis_kwargs, measurement, measurement_kwargs
+############################################################################
+
+def run(**kwargs):
+	sim.set_size(Lx, Ly, Lz, PARTICLES, DEVICES) 
+	sim.set_experiment(MODEL, KINETIC_OPERATOR, INIT, POTENTIAL, MEASUREMENT, EXP_KWARGS, POTENTIAL_KWARGS, MEASUREMENT_KWARGS)
+	sim.set_runtime(FRAME_SIZE, NUM_FRAMES)
+	sim.set_directory(BATCH, RUN, VISUALIZATION, **kwargs)
+	sim.init_GPU()
+	sim.simulate()
+	sim.visualize(VISUALIZATION, **VIS_KWARGS)
+	sim.animate(VISUALIZATION)
+
+def run_no_vis(**kwargs):
+	sim.set_size(Lx, Ly, Lz, PARTICLES, DEVICES) 
+	sim.set_experiment(MODEL, KINETIC_OPERATOR, INIT, POTENTIAL, MEASUREMENT, EXP_KWARGS, POTENTIAL_KWARGS, MEASUREMENT_KWARGS)
+	sim.set_runtime(FRAME_SIZE, NUM_FRAMES)
+	sim.set_directory(BATCH, RUN, VISUALIZATION, **kwargs)
+	sim.init_GPU()
+	sim.simulate()
+
+
+def vis_ani(file_name = '', **kwargs):
+	sim.set_size(Lx, Ly, Lz, PARTICLES, DEVICES) 
+	sim.set_experiment(MODEL, KINETIC_OPERATOR, INIT, POTENTIAL, MEASUREMENT, EXP_KWARGS, POTENTIAL_KWARGS, MEASUREMENT_KWARGS)
+	sim.set_runtime(FRAME_SIZE, NUM_FRAMES)
+	sim.set_directory(BATCH, RUN, VISUALIZATION, VIS_ONLY = True, **kwargs)
+	sim.visualize(VISUALIZATION, **VIS_KWARGS)
+	sim.animate(VISUALIZATION)
+
+def vis(file_name = '', **kwargs):
+	sim.set_size(Lx, Ly, Lz, PARTICLES, DEVICES) 
+	sim.set_experiment(MODEL, KINETIC_OPERATOR, INIT, POTENTIAL, MEASUREMENT, EXP_KWARGS, POTENTIAL_KWARGS, MEASUREMENT_KWARGS)
+	sim.set_runtime(FRAME_SIZE, NUM_FRAMES)
+	sim.set_directory(BATCH, RUN, VISUALIZATION, VIS_ONLY = True, **kwargs)
+	sim.visualize(VISUALIZATION, **VIS_KWARGS)
+
+
+def ani(file_name = '', **kwargs):
+	sim.set_size(Lx, Ly, Lz, PARTICLES, DEVICES) 
+	sim.set_experiment(MODEL, KINETIC_OPERATOR, INIT, POTENTIAL, MEASUREMENT, EXP_KWARGS, POTENTIAL_KWARGS, MEASUREMENT_KWARGS)
+	sim.set_runtime(FRAME_SIZE, NUM_FRAMES)
+	sim.set_directory(BATCH, RUN, VISUALIZATION, VIS_ONLY = True, **kwargs)
+	sim.animate(VISUALIZATION)
+
+### Not working yet
+def resume(file_name = '', **kwargs):
+	sim.set_size(Lx, Ly, Lz, PARTICLES, DEVICES) 
+	sim.set_experiment(MODEL, KINETIC_OPERATOR, INIT, POTENTIAL, MEASUREMENT, EXP_KWARGS, POTENTIAL_KWARGS, MEASUREMENT_KWARGS)
+	sim.set_runtime(FRAME_SIZE, NUM_FRAMES)
+	sim.set_directory(BATCH, RUN, VISUALIZATION, **kwargs)
+	sim.init_GPU()
+	sim.simulate()
+	sim.visualize(VISUALIZATION, **VIS_KWARGS)
+	sim.animate(VISUALIZATION)
+
---
+++ SimulationMaster.py
@@ -0,0 +1,99 @@
+#import Code.Simulations.SimulationHelper as sim
+import AbstractionLayer as QLGA
+
+######################################################################################
+############################# SIMULATION PARAMS ######################################
+######################################################################################
+
+#### LATTICE INFO ####
+# For particles >=2, (x,y,z) represents indices of number basis kets along each direction, ie. if xDim=L then XBLOCK*XGRID = L(2*L-1)
+PARTICLES = 2
+KINETIC_OPERATOR = 'S'   # S for schroedinger equation, D for dirac ... 
+FRAME_SIZE = 100
+NUM_FRAMES = 100
+
+
+Lx = 512 # Parameter to scale lattice size along x
+Ly = 1 # Parameter to scale lattice size along y
+Lz = 1 # Parameter to scale lattice size along z
+
+BATCH = '2P_test'
+RUN ='speed_optimizing'
+
+# ### Check devices
+# import pycuda.driver as drv
+# total_GPUs = drv.Device(0).count()
+# for GPU in xrange(total_GPUs):
+# 	print "Device " + str(GPU) + "=" + drv.Device(GPU).name()
+
+
+# quit()
+
+DEVICES = [1,2] # References to devices to use
+
+######################################################################################
+########################## SIMULATION PARAMS END #####################################
+######################################################################################
+
+
+############################# INITIAL CONDITIONS #####################################
+
+
+INIT = "entangled_gaussian_2P_1D"
+# INIT = 'double_quadrupole'
+#INIT = 'pade_quadrupole'
+
+
+############################ SIMULATION PHYSICS MODEL ################################
+
+# MODEL = 'spin2_BEC'
+MODEL = "No_Self_Interaction"
+
+
+
+############################ EXPERIMENT KEYWORDS #####################################
+
+
+#EXP_KWARGS = {'G0' : 1., 'G1' : .1, 'G2': 1., 'MU':1.,  'scaling' : 5, "solution1" : 5, "orientation1" : "x"} 
+# EXP_KWARGS = {'G0' : 1., 'G1' : .1, 'G2': 1., 'MU':1.,  'scaling' : 25, "solution2" : 2, "orientation2" : "x", "y_shift2" : 1./4.} 
+EXP_KWARGS = {'sigma1': 0.1, 'sigma2': 0.1, 'shift1': 0.35, 'shift2': 0.65,'sigma3': 0.1, 'sigma4': 0.1, 'shift3': 0.35, 'shift4': 0.65}
+
+
+###########################  EXTERNAL POTENTIAL #####################################
+
+
+POTENTIAL = "No_Potential"
+POTENTIAL_KWARGS = {}
+
+###########################  MEASUREMENT #####################################
+
+
+MEASUREMENT = "No_Measurement"
+MEASUREMENT_KWARGS = {}
+# MEASUREMENT = "Measurement"
+# MEASUREMENT_KWARGS = {'timestep' : 10, 'position' : 120, 'width' : 0, 'func' : "1./cosh", 'smooth' : False, 'Measured' : True}
+
+
+
+########################   VISUALIZATION TECHNIQUE #################################
+
+VISUALIZATION = '1D_2P'
+# VISUALIZATION = 'mayavi_2d_surface'
+# VISUALIZATION = 'mayavi_3d_isosurface' 
+
+
+VIS_KWARGS = {"fps":6}
+
+#####################################SETUP#######################################
+# ALWAYS RUN THIS
+QLGA.setup(PARTICLES, KINETIC_OPERATOR, FRAME_SIZE, NUM_FRAMES, Lx,  Ly, Lz, 
+			BATCH, RUN, DEVICES, INIT, MODEL, EXP_KWARGS, POTENTIAL, POTENTIAL_KWARGS,
+			VISUALIZATION, VIS_KWARGS, MEASUREMENT, MEASUREMENT_KWARGS)
+
+################################# EXPERIMENT  ########################################
+QLGA.run()
+# QLGA.run_no_vis()
+# QLGA.vis()
+# QLGA.ani()
+# QLGA.vis_ani()
+# QLGA.resume()
