214d1165a55b7b3754384d8dc48a443c6cad5d45
ssmith22 <ssmith22@hawaii.edu>
2019-07-19 15:28:20 -1000

    update

--- SimulationMaster.py
+++ SimulationMaster.py
@@ -7,24 +7,24 @@ import AbstractionLayer as QLGA
 
 #### LATTICE INFO ####
 # For particles >=2, (x,y,z) represents indices of number basis kets along each direction, ie. if xDim=L then XBLOCK*XGRID = L(2*L-1)
-PARTICLES = 2
-KINETIC_OPERATOR = 'S'   # S for schroedinger equation, D for dirac ... 
+PARTICLES = 1
+KINETIC_OPERATOR = 'S'   # S for Schroedinger equation, D for Dirac ... 
 FRAME_SIZE = 1
-NUM_FRAMES = 1
+NUM_FRAMES = 12
 
 
-Lx = 128 # Parameter to scale lattice size along x
-Ly = 128 # Parameter to scale lattice size along y
+Lx = 512 # Parameter to scale lattice size along x
+Ly = 1 # Parameter to scale lattice size along y
 Lz = 1 # Parameter to scale lattice size along z
 
 
-BATCH = '2p_test'
-RUN ='max_size_1D'
+BATCH = '1p_test'
+RUN ='measurement'
 
 
 
 
-DEVICES = [1,2,3,4] # References to devices to use
+DEVICES = [1,4] # References to devices to use
 
 
 ######################################################################################
@@ -35,7 +35,7 @@ DEVICES = [1,2,3,4] # References to devices to use
 ############################# INITIAL CONDITIONS #####################################
 
 
-INIT = "entangled_gaussian_2P_1D"
+INIT = "gaussians_1P_1D"
 # INIT = 'double_quadrupole_3d'
 #INIT = 'double_quadrupole'
 #INIT = 'pade_quadrupole'
@@ -54,8 +54,8 @@ MODEL = "No_Self_Interaction"
 
 # EXP_KWARGS = {'G0' : 1., 'G1' : .1, 'G2': 1., 'MU':1.,  'scaling' : 5, "solution1" : 5, "orientation1" : "x"} 
 # EXP_KWARGS = {'G0' : 1., 'G1' : .1, 'G2': 1., 'MU':1.,  'scaling' : 25, "solution2" : 2, "orientation2" : "x", "y_shift2" : 1./4.} 
-EXP_KWARGS = {'sigma1': 0.05, 'sigma2': 0.05, 'shift1': 0.35, 'shift2': 0.65,'sigma3': 0.05, 'sigma4': 0.05, 'shift3': 0.35, 'shift4': 0.65, 'cond_list':["X1==X2","true"],'func_list':["0.","(pi/10.)*(1./abs(X1-X2))"]}
-
+EXP_KWARGS = {'momentums':[0.,0.], "shifts" : [0.25,0.75], "sigmas" : [0.03, 0.03]}
+### 'cond_list':["X1==X2","true"],'func_list':["0.","(pi/2.)*(1./abs(X1-X2))"] coulomb
 
 ###########################  EXTERNAL POTENTIAL #####################################
 
@@ -63,21 +63,23 @@ EXP_KWARGS = {'sigma1': 0.05, 'sigma2': 0.05, 'shift1': 0.35, 'shift2': 0.65,'si
 POTENTIAL = "No_Potential"
 POTENTIAL_KWARGS = {}
 # POTENTIAL = "External_Function"
-# POTENTIAL_KWARGS = {"cond_list":["true"],"func_list" : ["((pi/10.)/(Lx*Lx))*((X1-Lx/2.)*(X1-Lx/2.)+(X2-Lx/2.)*(X2-Lx/2.))"]} # Put cuda style function in string format, parameters 1P:(X,Y,Z,Y,Lx,Ly,Lz), 2P:(X1,Y1,Z1,X2,Y2,Z2,Lx,Ly,Lz)
+## double trap POTENTIAL_KWARGS = {"cond_list":["X1<Lx/2. && X2<Lx/2.","X1>=Lx/2. && X2>=Lx/2.","X1<Lx/2. && X2>=Lx/2.", "X1>=Lx/2. && X2<Lx/2."],"func_list" : ["((pi/20.)/(Lx*Lx))*((X1-Lx/4.)*(X1-Lx/4.)+(X2-Lx/4.)*(X2-Lx/4.))","((pi/20.)/(Lx*Lx))*((X1-3.*Lx/4.)*(X1-3.*Lx/4.)+(X2-3.*Lx/4.)*(X2-3.*Lx/4.))","((pi/20.)/(Lx*Lx))*((X1-Lx/4.)*(X1-Lx/4.)+(X2-3.*Lx/4.)*(X2-3.*Lx/4.))","((pi/20.)/(Lx*Lx))*((X1-3.*Lx/4.)*(X1-3.*Lx/4.)+(X2-Lx/4.)*(X2-Lx/4.))"]} 
+# single trap POTENTIAL_KWARGS = {"cond_list":["true"],"func_list" : ["((pi/40.)/(Lx*Lx))*((X1-Lx/2.)*(X1-Lx/2.)+(X2-Lx/2.)*(X2-Lx/2.))"]}
 
 ###########################  MEASUREMENT #####################################
 
 
-MEASUREMENT = "No_Measurement"
-MEASUREMENT_KWARGS = {}
-# MEASUREMENT = "Measurement"
-# MEASUREMENT_KWARGS = {'timestep' : 10, 'position' : 120, 'width' : 0, 'func' : "1./cosh", 'smooth' : False, 'Measured' : True}
+# MEASUREMENT = "No_Measurement"
+# MEASUREMENT_KWARGS = {}
+MEASUREMENT = "Measurement_1D"
+MEASUREMENT_KWARGS = {'timestep' : 10, 'position' : 120, 'width' : 0, 'smooth' : False, 'Measured' : True}
 
 
 
 ########################   VISUALIZATION TECHNIQUE #################################
 
-VISUALIZATION = '1D_2P'
+VISUALIZATION = '1D_1P'
+# VISUALIZATION = '1D_2P'
 # VISUALIZATION = 'mayavi_2d_surface'
 # VISUALIZATION = 'mayavi_3d_isosurface' 
 
@@ -91,8 +93,8 @@ QLGA.setup(PARTICLES, KINETIC_OPERATOR, FRAME_SIZE, NUM_FRAMES, Lx,  Ly, Lz,
 			VISUALIZATION, VIS_KWARGS, MEASUREMENT, MEASUREMENT_KWARGS)
 
 ################################# EXPERIMENT  ########################################
-# QLGA.run()
-QLGA.run_no_vis()
+QLGA.run()
+# QLGA.run_no_vis()
 # QLGA.vis()
 # QLGA.ani()
 # QLGA.vis_ani()
